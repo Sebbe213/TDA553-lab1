f7c5d5c96c4eeb065f361de6d1fe64c11daa8988
Sebbe213 <sebastianraoof@hotmail.se>
2022-11-18 10:56:04 +0100

    test

--- test/set/CarsTest.java
+++ test/set/CarsTest.java
@@ -1,8 +1,12 @@
 package test.set;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.Test;
 
 public class CarsTest {
     @Test
-    public void 
+    public void test_how_the_car_turns_left(){
+        Saab95 saab = new Saab95;
+    }
 }
