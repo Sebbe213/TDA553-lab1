ee076c2ecff40cdf82d7f310efd8e1dd090abc64
Gabriel2000001 <radwanr@net.chalmers.se>
2022-11-18 11:38:17 +0100

    setTurboOn tests is complete.

---
+++ test/Saab95Test.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+public class Saab95Test {
+
+    @Test
+    public void setTurboOn() {
+        Saab95 mySebbe = new Saab95(2, Color.WHITE,125,"Volvo240",0.1,10,10,0,0);
+
+        assertTrue(mySebbe.turboOn);
+    }
+
+
+    @Test
+    public void setTurboOff() {
+        Saab95 mySebbe = new Saab95(2, Color.WHITE,125,"Volvo240",0.1,10,10,0,0);
+
+        assertFalse(mySebbe.turboOn);
+
+    }
+
+    @Test
+    public void speedFactor() {
+        // HEEEEEEEEEEEEEEEEEEEEj
+
+    }
+
+    @Test
+    public void brake() {
+    }
+}
