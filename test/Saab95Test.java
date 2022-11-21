d76732c51ac2644d6d3ac0aecf7cea9d92c0f029
Gabriel2000001 <radwanr@net.chalmers.se>
2022-11-21 13:34:34 +0100

    Test are done now

--- test/Saab95Test.java
+++ test/Saab95Test.java
@@ -1,5 +1,3 @@
-
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.awt.*;
@@ -9,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class Saab95Test {
     @Test
     public void speedFactorTest() {
-        Saab95 saab = new Saab95(2, Color.WHITE, 125, "Saab95", 0, 10);
+        Saab95 saab = new Saab95(2, Color.WHITE,125,"Saab95",0.1,10);
         assertEquals(1.625,saab.speedFactor(20));
     }
 
@@ -17,27 +15,18 @@ public class Saab95Test {
 
     @Test
     public void incrementSpeedTest() {
-        Saab95 saab = new Saab95(2, Color.WHITE, 125, "Saab95", 0, 10);
+        Saab95 saab = new Saab95(2, Color.WHITE,125,"Saab95",0.1,0);
         saab.incrementSpeed(10);
-        assertEquals(16.35,saab.getCurrentSpeed());
+        assertEquals(26.25,saab.getCurrentSpeed());
 
 
     }
 
     @Test
     public void decrementSpeedTest() {
-        Saab95 saab = new Saab95(2, Color.WHITE, 125, "Saab95", 0, 10);
+        Saab95 saab = new Saab95(2, Color.WHITE,125,"Saab95",0.1,0);
         saab.decrementSpeed(10);
-        assertEquals(-16.15,saab.getCurrentSpeed());
-
-    }
-
-    @Test
-    public void gasTest() {
-        Saab95 saab = new Saab95(2, Color.WHITE, 125, "Saab95", 0, 10);
-    
-
-
+        assertEquals(-6.25,saab.getCurrentSpeed());
     }
 
     @Test
@@ -46,43 +35,30 @@ public class Saab95Test {
 
     @Test
     public void turnRightTest(){
-        Saab95 saab = new Saab95(2, Color.WHITE, 125, "Saab95", 0, 10);
-        saab.getxVelocity();
-        // saab.setyCoordination();
-        assertEquals(-0.1, saab.getxVelocity());
-        // assertEquals(0, saab.getyCoordination());
-
-
-
-    }
-
-    @Test
-    public void turnLeftTest(){
-        Saab95 saab = new Saab95(2, Color.WHITE, 125, "Saab95", 0, 10);
+        Saab95 saab = new Saab95(2,Color.WHITE,125, "Saab95", 10,0);
+        saab.turnRight();
         saab.setxCoordination();
+        assertEquals(0,saab.getxCoordination());
+    }
+    @Test
+    public void turnLeftTest() {
+        Saab95 saab = new Saab95(4, Color.WHITE, 125, "Saab404", 0, 10);
+        saab.turnLeft();
         saab.setyCoordination();
-        assertEquals(null, saab.getxCoordination());
-        assertEquals(null, saab.getyCoordination());
+        assertEquals(0, saab.getyCoordination());
+
     }
-
-    /*
     @Test
-    public void setTurboOn() {
-        Saab95 mySebbe = new Saab95(2, Color.WHITE,125,"Volvo240",0.1,10,10,0,0);
-
-        assertTrue(mySebbe.turboOn);
+    public void gasTest() {
+        Saab95 saab = new Saab95(4,Color.WHITE, 125,"Saab404", 0,10);
+        saab.gas(0.5);
+        assertEquals(10.8125, saab.getCurrentSpeed());
     }
-
-
     @Test
-    public void setTurboOff() {
-        Saab95 mySebbe = new Saab95(2, Color.WHITE,125,"Volvo240",0.1,10,10,0,0);
-
-        assertFalse(mySebbe.turboOn);
-
-     */
-
-
-
+    public void brakTest() {
+        Saab95 saab = new Saab95(4,Color.WHITE, 125,"Saab404", 0,10);
+        saab.brake(0.5);
+        assertEquals(9.1875,saab.getCurrentSpeed());
+    }
 
 }
