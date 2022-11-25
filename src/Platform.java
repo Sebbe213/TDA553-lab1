da25556be629477e3aaa626b011e22d677be71ad
Sebbe213 <sebastianraoof@hotmail.se>
2022-11-25 11:10:53 +0100

    changed Platform

--- src/Platform.java
+++ src/Platform.java
@@ -1,10 +1,26 @@
-public class Platform {
-    protected double angel;
 
-    protected Platform(double angel) {
+public class Platform implements iPlatformInterface, Ramp_level {
+    protected  double angel;
+    Ramp_level.Level platform;
+    
+    
+
+    protected Platform(Ramp_level.Level platform) {
+        this.platform = platform;
         this.angel = angel;
     }
-    protected void test() {
-        Platform plattan = new Platform()
-    }
+    public void lower(){
+        if(angel >= 0 && angel < 70){
+        platform=Level.DOWN;
+        }
+       }
+      public void raise() {
+    if(angel<= 70 && angel > 0 ){
+            platform = Level.UP;
+      }
+}
+      public void getRaise(){
+
+
+}
 }
