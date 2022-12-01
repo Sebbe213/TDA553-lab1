9238b583fd280135b27a5c76df11f2203486bf27
Gabriel2000001 <radwanr@net.chalmers.se>
2022-12-01 14:40:24 +0100

    load method is partially done.

--- src/transportCar.java
+++ src/transportCar.java
@@ -2,9 +2,17 @@ import java.awt.*;
 
 public class transportCar extends Car {
     private int maxCapcity;
+    private int nrOfCarsToLoad;
+    private int closnesIntervall;
+
+    private int loadedCars;
+
+    private int nrOfAvailableSlots;
+
 
     Ramp transportCarramp = new Ramp(true);
-    protected transportCar(int nrDoors, Color color, double enginePower, String modelName, double currentSpeed, double xVelocity, double yVelocity,int maxCapcity) {
+
+    protected transportCar(int nrDoors, Color color, double enginePower, String modelName, double currentSpeed, double xVelocity, double yVelocity, int maxCapcity) {
         super(nrDoors, color, enginePower, modelName, currentSpeed, xVelocity, yVelocity);
         int maxCapacity = maxCapcity;
 
@@ -17,11 +25,34 @@ public class transportCar extends Car {
 
     @Override
     protected void incrementSpeed(double amount) {
-
     }
 
     @Override
     protected void decrementSpeed(double amount) {
+    }
 
+    protected void load() {
+        if (closnesIntervall <= 20 && nrOfCarsToLoad <= maxCapcity - nrOfCarsToLoad) {
+            nrOfAvailableSlots -= nrOfCarsToLoad;
+
+
+        }
+
+    }
+
+    protected int getLoadedCars() {
+        return nrOfCarsToLoad;
+    }
+
+    protected void setLoadedCars() {
+
+    }
+
+    protected int getNrOfAvailableSlots() {
+        return nrOfAvailableSlots;
+    }
+
+    protected void setNrOfAvailableSlots() {
+        nrOfAvailableSlots -= loadedCars;
     }
 }
