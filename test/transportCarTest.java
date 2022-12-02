58844b65e3e534d54fcb316544c4c562b8a18a32
AkramEl46 <elkhoulti02@gmail.com>
2022-12-02 14:41:05 +0100

    test transportCarTest

--- test/transportCarTest.java
+++ test/transportCarTest.java
@@ -2,30 +2,32 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.awt.Color;
+
 class transportCarTest {
 
     @Test
     void loadOn() {
-        transportCar car = new transportCar(0, null, 0, null, 0, 0, 0, 0);
-        Saab95 saab = new Saab95(0, null, 0, null, 0, 0, 0)
-        car.loadOn(saab); 
-        assertEquals((1,loadedCarList.size()));
+        transportCar car = new transportCar(4, Color.WHITE, 90, "charge", 0, 10, 0, 3);
+        Saab95 saab = new Saab95(2, Color.WHITE, 125, "Saab95", 0,10, 0);
+        car.loadOn(saab);
+        car.loadedCarList.add(saab);
+        car.updateNrOfAvailableSlots();
+        assertEquals(2,car.getNrOfAvailableSlots());
        
     }
 
     @Test
     void loadOff() {
-        transportCar car = new transportCar(0, null, 0, null, 0, 0, 0, 0);
-        car.loadOff(); 
-        assertEquals(null, null);
+        transportCar car = new transportCar(4, Color.WHITE, 90, "charge", 0, 10, 0, 3);
+        Saab95 saab = new Saab95(2, Color.WHITE, 125, "Saab95", 0,10, 0);
+        car.loadOff(saab);
+        car.loadedCarList.remove(saab);
+        car.updateNrOfAvailableSlots();
+        assertEquals(3,car.getNrOfAvailableSlots());
     }
-    @Test
-    boolean xCoordinationChecker(){
-        transportCar car = new transportCar(0, null, 0, null, 0, 0, 0, 0);
-        
-        assertEquals(null, null);
+    
+   
 
-        // Hej
-
-    }
+    
 }
