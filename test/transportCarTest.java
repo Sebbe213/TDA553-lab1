55ad7c3625ee7efd8a46bcf6685f50f4d0d26318
Gabriel2000001 <radwanr@net.chalmers.se>
2022-12-02 10:58:32 +0100

    A new test file for the transport car class is added.

---
+++ test/transportCarTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class transportCarTest {
+
+    @Test
+    void loadOn() {
+    }
+
+    @Test
+    void loadOff() {
+    }
+}
