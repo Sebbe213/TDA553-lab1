40567d81788e1bcff85c04dba916891216c75cb7
AkramEl46 <elkhoulti02@gmail.com>
2022-12-09 11:34:37 +0100

    h

--- src/Lab2/Scania.java
+++ src/Lab2/Scania.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 public class Scania extends Car {
 
-    Platform platta = new Platform(rear.Level.UP, 0);
+    Platform platta = new Platform(rear.Level.DOWN, 0);
 
     public Scania(int nrDoors, Color color, double enginePower, String modelName, double currentSpeed, double xVelocity, double yVelocity, double xCoordination, double  yCoordination) {
         super(2, Color.gray, 90, "Scania Model S", currentSpeed, xVelocity, yVelocity,false,xCoordination ,yCoordination);
