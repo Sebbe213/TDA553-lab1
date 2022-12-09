e6a744fcd7797764e60710cab1e82bf7f42b7d12
AkramEl46 <elkhoulti02@gmail.com>
2022-12-09 10:39:07 +0100

    auick

--- src/Lab2/TransportCar.java
+++ src/Lab2/TransportCar.java
@@ -60,3 +60,4 @@ public class TransportCar extends Car {
     }
 }
 
+
--- test/Lab2_test/ScaniaTest.java
+++ test/Lab2_test/ScaniaTest.java
@@ -32,7 +32,7 @@ class ScaniaTest {
 
     @Test
     void shouldNotRaisePlatformIfCurrentSpeedGreaterThanZero() {
-        Scania scaniaS = new Scania(2, Color.gray, 90, "Scania Model S", 0, 0, 0,0,0);
+        Scania scaniaS = new Scania(2, Color.gray, 90, "Scania Model S", 10, 0, 0,0,0);
         scaniaS.raisePlatform(20);
         assertEquals(0, scaniaS.platta.getAngle());
 
@@ -40,7 +40,7 @@ class ScaniaTest {
 
     @Test
     void shouldIncrementSpeedOnlyIfPlatforIsUp() {
-        Scania scaniaS = new Scania(2, Color.gray, 90, "Scania Model S", 0, 0, 0,0,0);
+        Scania scaniaS = new Scania(2, Color.gray, 90, "Scania Model S", 10, 0, 0,0,0);
         scaniaS.startEngine();
         scaniaS.incrementSpeed(10);
         assertEquals(10.1, scaniaS.getCurrentSpeed());
